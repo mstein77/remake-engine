d43463eb90bb774ad86b51ed6299cc71ad175676
Marc Stein <marc.stein@BADL-ASC02PJ18RG3QN.fritz.box>
2019-07-26 22:31:16 +0200

    Basic Webpack Structure and Babel for ES6

---
+++ src/app/engine.js
@@ -0,0 +1,428 @@
+var OffscreenCanvasManager = function() {
+    return {
+        canvasElems: [],
+
+        getNewCanvas: function (dimX, dimY) {
+            var id = 'offcan_' + this.canvasElems.length;
+            var offElem = document.getElementById('offscreen');
+            offElem.innerHTML +=
+                '<canvas id="' + id + '" width="' + dimX + '" height="' + dimY + '"></canvas>';
+            var elem = document.getElementById(id);
+            var canvas = {id: id, elem: elem, ctx: elem.getContext('2d')};
+            this.canvasElems.push(canvas);
+            return canvas;
+        }
+    };
+};
+
+var OCM = new OffscreenCanvasManager();
+
+var PatternPane = function(pattern, repeat) {
+    return {
+        canvas: null,
+        pattern: pattern,
+        repeat: repeat,
+
+        init: function (width, height) {
+            this.patternDimX = this.pattern.width;
+            this.patternDimY = this.pattern.height;
+            var dimX = width + this.patternDimX;
+            var dimY = height + this.patternDimY;
+            this.canvas = OCM.getNewCanvas(dimX, dimY);
+            this.canvas.ctx.fillStyle = '"rgba(0, 0, 0, 1)"';
+            this.canvas.ctx.fillRect(0, 0, dimX, dimY);
+            if (true) {
+                // opaque pattern
+                var pattern = this.canvas.ctx.createPattern(this.pattern, this.repeat);
+                this.canvas.ctx.fillStyle = pattern;
+                this.canvas.ctx.fillRect(0, 0, dimX, dimY);
+            } else {
+                // transparent pattern
+                var y = 0;
+                while (y < dimY) {
+                    var x = 0;
+                    while (x < dimX) {
+                        this.canvas.ctx.drawImage(this.pattern, x, y);
+                        x += this.patternDimX;
+                    }
+                    y += this.patternDimY;
+                }
+            }
+            this.width = width;
+            this.height = height;
+            this.image = null;
+            this.scrollX = 0;
+            this.scrollY = 0;
+        },
+
+        getImage: function () {
+            if (this.image === null) {
+                this.image =
+                    this.canvas.ctx.getImageData(this.scrollX, this.scrollY, this.width, this.height);
+            }
+            return this.image;
+        },
+
+        render: function (target) {
+            target.putImageData(this.getImage(), 0, 0);
+        },
+
+        scrollBy: function (Sx, Sy) {
+            this.scrollX += Sx;
+            if (this.scrollX < 0) {
+                this.scrollX += this.patternDimX;
+            } else if (this.scrollX >= this.patternDimX) {
+                this.scrollX -= this.patternDimX;
+            }
+            this.scrollY += Sy;
+            if (this.scrollY < 0) {
+                this.scrollY += this.patternDimY;
+            } else if (this.scrollY >= this.patternDimY) {
+                this.scrollY -= this.patternDimY;
+            }
+            this.image = null;
+        }
+    }
+};
+
+var WorldPane = function(tb, tiles, world, bgPane) {
+    return {
+        tb: tb,
+        tl: 1 << tb,
+        spriteSheet: tiles,
+        world: world,
+        worldX: 0,
+        worldY: 0,
+        VOy: 0,
+        VOx: 0,
+        bgPane: bgPane,
+        canvas: null,
+
+        init: function (width, height, offscreen) {
+            this.dimX = width;
+            this.dimY = height;
+            this.viewWidth = Math.ceil(width / this.tl);
+            this.viewHeight = Math.ceil(height / this.tl);
+            this.worldMaxY = Math.max(this.world.length - this.viewHeight, 0);
+            this.worldMaxX = Math.max(this.world[0].length - this.viewWidth, 0);
+            this.VOMaxY = height % this.tl;
+            this.VOMaxX = width % this.tl;
+            this.offCanvas = (offscreen === true) ? OCM.getNewCanvas(width, height) : null;
+        },
+
+        render: function (drawTarget) {
+            var target = this.offCanvas !== null ? this.offCanvas.ctx : drawTarget;
+            if (this.bgPane !== undefined) {
+                this.bgPane.render(target);
+                target.fillRect(0, 0, this.dimX, this.dimY);
+            }
+            /*
+             else {
+                target.clearRect(0, 0, this.dimX, this.dimY);
+            }
+            */
+
+            var x_max = Math.min(this.worldX + this.viewWidth + 1, this.world[0].length);
+            var y_max = Math.min(this.worldY + this.viewHeight + 1, this.world.length);
+            d('View-Size', this.viewWidth, 'x', this.viewHeight);
+            d('VOMaxY', this.VOMaxY);
+            d('View-Offset', this.VOx, '/', this.VOy);
+            d('World-Size', this.world[0].length, 'x', this.world.length);
+            d('WorldMaxY', this.worldMaxY);
+            d('World-Pos', this.worldX, '/', this.worldY);
+
+            j = 0;
+            for (y = this.worldY; y < y_max; y++) {
+                i = 0;
+                for (x = this.worldX; x < x_max; x++) {
+                    var tile = this.world[y][x];
+                    if (tile === 0) {
+                        i++;
+                        continue;
+                    }
+                    target.drawImage(
+                        this.spriteSheet,
+                        tile << this.tb,
+                        0,
+                        this.tl,
+                        this.tl,
+                        -this.VOx + (i << this.tb),
+                        -this.VOy + (j << this.tb),
+                        this.tl,
+                        this.tl
+                    );
+                    i++
+                }
+                j++;
+            }
+
+            if (this.offCanvas !== null) {
+                var im = target.getImageData(0, 0, this.dimX, this.dimY);
+                drawTarget.putImageData(im, 0, 0);
+            }
+        },
+
+        scrollBy: function (Sx, Sy) {
+            var unscrolled = {
+                x: 0,
+                y: 0
+            };
+            if (Sy > 0) {
+                // scroll down
+                this.VOy += Sy;
+
+                if (this.VOy >= this.tl) {
+                    this.VOy -= this.tl;
+                    if (this.worldY < this.worldMaxY) {
+                        this.worldY++;
+                    }
+                }
+
+                if (this.worldY === this.worldMaxY) {
+                    if (this.VOy > this.VOMaxY) {
+                        unscrolled.y = Math.abs(this.VOy - this.VOMaxY);
+                        this.VOy = this.VOMaxY;
+                    }
+                }
+
+            } else if (Sy < 0) {
+                // scroll up
+                this.VOy += Sy;
+
+                if (this.VOy < 0) {
+                    if (this.worldY > 0) {
+                        this.worldY--;
+                        this.VOy += this.tl;
+                    } else {
+                        unscrolled.y = this.VOy;
+                        this.VOy = 0;
+                    }
+                }
+            }
+
+            if (Sx > 0) {
+                // scroll right
+                this.VOx += Sx;
+
+                if (this.VOx >= this.tl) {
+                    this.VOx -= this.tl;
+                    if (this.worldX < this.worldMaxX) {
+                        this.worldX++;
+                    }
+                }
+
+                if (this.worldX === this.worldMaxX) {
+                    if (this.VOx > this.VOMaxX) {
+                        unscrolled.x = this.VOx - this.VOMaxX;
+                        this.VOx = this.VOMaxX;
+                    }
+                }
+
+            } else if (Sx < 0) {
+                // scroll left
+                this.VOx += Sx;
+
+                if (this.VOx < 0) {
+                    if (this.worldX > 0) {
+                        this.worldX--;
+                        this.VOx += this.tl;
+                    } else {
+                        unscrolled.x = this.VOx;
+                        this.VOx = 0;
+                    }
+                }
+            }
+            return unscrolled;
+        }
+    };
+}
+
+var LinearGradientBackground = function(axis, pViewSize) {
+    return {
+        colorStops: [],
+        colorStopIndex: null,
+        colorStopPosition: null,
+        viewPosition: null,
+        viewPositionMax: null,
+        viewSize: pViewSize,
+        isHorizontal: (axis === 'X'),
+
+        addColorStop: function(color, size) {
+            this.colorStops.push([color, size]);
+        },
+
+        init: function() {
+            this.colorStopIndex = 0;
+            this.colorStopPosition = 0;
+            this.viewPosition = 0;
+            var totalSize = 0;
+
+            for (var i=0; i < this.colorStops.length; i++) {
+                totalSize += this.colorStops[i][1];
+            }
+            this.viewPositionMax = totalSize - (this.viewSize + 1);
+        },
+
+        render: function(bgCtx) {
+            d('Index', this.colorStopIndex, '| Position', this.colorStopPosition);
+            d('View: ', this.viewPosition, ' of ', this.viewPositionMax);
+
+            var i = this.colorStopIndex;
+            var i_max = this.colorStops.length;
+            var pos = this.colorStopPosition - this.viewPosition;
+            var posStart = pos;
+            var posMax = this.viewPosition + this.viewSize - 1;
+
+            var len = 0;
+            // wir suche jetzt den letzten relevanten ColorStop für den View
+            while (pos < posMax && i < i_max) {
+                var stop = this.colorStops[i];
+                pos += stop[1];
+                len += stop[1];
+                i++;
+            }
+
+            if (len === 0) {
+                return;
+            }
+
+            var gradient = this.isHorizontal ?
+                bgCtx.createLinearGradient(posStart, 0, pos, 0) :
+                bgCtx.createLinearGradient(0, posStart, 0, pos);
+
+            var faktor = 1 / len;
+            pos = 0;
+            for (var j = this.colorStopIndex; j <= i; j++) {
+                if (j >= i_max) {
+                    break;
+                }
+                var stop = this.colorStops[j];
+                gradient.addColorStop(pos, stop[0]);
+                pos += stop[1] * faktor;
+            }
+
+            bgCtx.fillStyle = gradient;
+        },
+
+        scrollBy: function(speed) {
+            if (speed === 0) {
+                return;
+            }
+            var newPos = this.viewPosition + speed;
+            if (speed > 0) {
+                if (newPos > this.viewPositionMax) {
+                    newPos = this.viewPositionMax;
+                }
+
+                if (newPos !== this.viewPosition) {
+                    var posEnd = this.colorStopPosition + this.colorStops[this.colorStopIndex][1];
+                    while(posEnd < newPos) {
+                        this.colorStopPosition = posEnd;
+                        this.colorStopIndex++;
+                        posEnd += this.colorStops[this.colorStopIndex][1];
+                    }
+                }
+
+            } else {
+                if (newPos < 0) {
+                    newPos = 0;
+                }
+
+                if (newPos !== this.viewPosition) {
+                    while (this.colorStopPosition > newPos) {
+                        this.colorStopIndex--;
+                        this.colorStopPosition -= this.colorStops[this.colorStopIndex][1]
+                    }
+                }
+
+            }
+            this.viewPosition = newPos;
+        }
+    };
+};
+
+function printDebugs() {
+    var out = [];
+    out.push("Debug:");
+    out.push("===============================================");
+    for (k in debugs) {
+        out.push(k + ': ' + debugs[k])
+    }
+    debugElem.innerHTML = out.join("\n");
+    debugs = [];
+}
+
+function d() {
+    if (arguments.length === 0) {
+        return;
+    }
+    var key = arguments[0];
+    var values = [];
+    for (i = 1; i < arguments.length; i++) {
+        values.push('' + arguments[i]);
+    }
+    debugs[key] = values.join(' ');
+}
+
+function SpriteMap(ctx, bits) {
+    var len = 1 << (bits - 1);
+    var sprite = ctx.createImageData(len, len);
+    return {
+        im: sprite,
+        len: len,
+        bits: bits,
+        row: len << 2,
+        cols: [],
+        makeTransparent: function () {
+            i = 3;
+            var d = this.im.data;
+            i_max = d.length;
+            while(i < i_max) {
+                d[i] = 0;
+                i += 4;
+            }
+        },
+        addCol: function (r, g, b, a) {
+            this.cols.push([r, g, b, a]);
+        },
+        set: function (x, y, col) {
+            var i = y * this.row + (x << 2);
+            var c = this.cols[col];
+            var d = this.im.data;
+            d[i] = c[0];
+            d[++i] = c[1];
+            d[++i] = c[2];
+            d[++i] = c[3];
+        }
+    }
+}
+
+function getNewSpriteMap(ctx, bits) {
+    return new SpriteMap(ctx, bits);
+}
+
+function getNewSprite(mapKey, x, y) {
+    if (spriteMaps[mapKey] === undefined) {
+        throw Error('Unknown SpriteMap key ' + mapKey);
+    }
+    return {im: spriteMaps[mapKey].im, x: x, y: y, len: spriteMaps[mapKey].len};
+}
+
+var debugElem = document.getElementById('d');
+var debugs = [];
+var spriteMaps = [];
+
+
+module.exports = {
+    PatternPane,
+    WorldPane,
+    LinearGradientBackground,
+    printDebugs,
+    d,
+    SpriteMap,
+    getNewSpriteMap,
+    getNewSprite,
+    spriteMaps
+};
+
+
---
+++ src/app/index.js
@@ -0,0 +1,234 @@
+import {
+    PatternPane,
+    WorldPane,
+    LinearGradientBackground,
+    printDebugs,
+    d,
+    getNewSpriteMap,
+    getNewSprite,
+    spriteMaps
+} from './engine.js';
+
+var canvas = document.getElementById('c');
+var ctx = canvas.getContext('2d');
+
+var sprites = [];
+var keys = {};
+
+var tb = 4;
+
+var spriteSheet = new Image();
+spriteSheet.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAQCAYAAABQrvyxAAAA9klEQVRIS2NkoBLYdejVf1KMcrUVBSt/oqNDijYMtYwU6YZqTsyc9P/tLzuSjdo4Rx/sAdmrV8lyx2Nt7f9kaUR3KcgDD58zkeQBHmEbhiHvgZx4KQbNTMfBEQOkJKEvb48wgGJgUHng/rHvRCUhPhN3hlEPQIOKqpkYFAMHLpbjLBQc9DvBxeygjwFcnhgyHgCFMjZPDAkPgJLHpzM7wSkU3RNDwgNVkxMZpix8htUTQ8ID+y+UMew+/BruCSZFbnBs7NuQwzgkPIBeEcA88O/+V7jUoCyFQK4DtYeIqsmQFM2fnscIKssHvDFHqsOR1YM8QIl+ALtc7JEpDo/lAAAAAElFTkSuQmCC";
+var world = [
+    [2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [2, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+var levelPane = new WorldPane(tb, spriteSheet, world);
+var fadeBg = new LinearGradientBackground('Y', canvas.height);
+fadeBg.addColorStop('#000000', 100);
+fadeBg.addColorStop('#400000', 100);
+fadeBg.addColorStop('#000080', 100);
+fadeBg.addColorStop('#F0F040', 100);
+fadeBg.addColorStop('#802050', 100);
+fadeBg.init();
+
+var bgPane = new WorldPane(
+    tb,
+    spriteSheet,
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    , fadeBg
+);
+
+var player0 = new Image();
+player0.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAA7ElEQVRYR9WX0Q6FIAxD2f9/NAYTDE5g7ZQFfbk+SHuAsnEl55xTSklEpPxGPZdvfYmEuHkW40gI7XUtewREz+O27yshRtqP4K2AmGl2k/8lhKU1PHrWQOS4IhrTs48IjEDQsWbxQYVaEGaMCcDWCcb8LH7IXqIQrDkFYEF4zGmAEYTX3AWgIdot9HRUOAM6K+2s33TSfwLo2dfVCdmCXuDCQjgz8kLAGUAMkG90mCEARpj5FqoDrKBVMakV8JhXA3TsnhcSlB7ppJbWXpdSixaZMXtF2+OPycqZz7po6R0Sad49ohXA08m+yMQB423wEY5FdSYAAAAASUVORK5CYII=";
+
+var patternBg = new PatternPane(player0, 'repeat');
+
+function prepareSpriteMaps() {
+
+    var player = getNewSpriteMap(ctx, 6);
+    player.makeTransparent();
+    player.addCol(255, 230, 220, 255);
+    for (var i = 0; i < player.len; i++) {
+        player.set(i, i, 0);
+        player.set(player.len - (i + 1), i, 0);
+    }
+    spriteMaps['x'] = player;
+}
+
+function init() {
+    prepareSpriteMaps();
+    var player = getNewSprite('x', 30, 20);
+    sprites.push(player);
+    levelPane.init(canvas.width, canvas.height);
+    bgPane.init(canvas.width, canvas.height, false);
+    patternBg.init(canvas.width, canvas.height);
+}
+
+function render() {
+    bgPane.render(ctx);
+//        patternBg.render(ctx);
+    levelPane.render(ctx);
+
+    sprites.forEach(
+        function (v) {
+            ctx.drawImage(player0, v.x, v.y);
+        }
+    );
+    patternBg.scrollBy(2, 1);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    if (sprites.length === 0) {
+        init();
+    }
+    handleKeys();
+    render();
+    printDebugs();
+}
+
+onload = update;
+
+function handleKeys() {
+    var moveX = 0;
+    var moveY = 0;
+    var speed = 3;
+    for (var key in keys) {
+        switch (key) {
+            case 'a':
+                moveX -= speed;
+                break;
+
+            case 'd':
+                moveX += speed;
+                break;
+
+            case 'w':
+                moveY -= speed;
+                break;
+
+            case 's':
+                moveY += speed;
+                break;
+        }
+    };
+
+    var scrollBoundsTop = {x: 130, y: 50};
+    var scrollBoundsBottom = {x: 130, y: 50};
+
+    var scrollX = 0;
+    if (moveX !== 0) {
+        var pos = sprites[0].x + moveX;
+        if (pos < scrollBoundsTop.x) {
+            // new position is left of scrollbounds
+            if (sprites[0].x >= scrollBoundsTop.x) {
+                scrollX = -Math.abs(scrollBoundsTop.x - pos);
+                pos = scrollBoundsTop.x;
+            } else if (pos < 0) {
+                pos = 0;
+            }
+        }
+
+        var max = canvas.width - 1 - sprites[0].len;
+        var rightScrollBound = max - scrollBoundsBottom.x;
+        if (pos > rightScrollBound) {
+            if (sprites[0].x <= rightScrollBound) {
+                scrollX = Math.abs(pos - rightScrollBound);
+                pos = rightScrollBound;
+            } else if (pos > max) {
+                pos = max;
+            }
+        }
+        sprites[0].x = pos;
+    }
+    var scrollY = 0;
+    if (moveY !== 0) {
+        var pos = sprites[0].y + moveY;
+        if (pos < scrollBoundsTop.y) {
+
+            if (sprites[0].y >= scrollBoundsTop.y) {
+                scrollY = -Math.abs(scrollBoundsTop.y - pos);
+                pos = scrollBoundsTop.y;
+            } else if (pos < 0) {
+                pos = 0;
+            }
+        }
+        var max = canvas.height - 1 - sprites[0].len;
+        var bottomScrollBound = max - scrollBoundsBottom.y;
+        if (pos > bottomScrollBound) {
+            if (sprites[0].y <= bottomScrollBound) {
+                scrollY = Math.abs(pos - bottomScrollBound);
+                pos = bottomScrollBound;
+            } else if (pos > max) {
+                pos = max;
+            }
+        }
+        sprites[0].y = pos;
+    }
+
+
+    fadeBg.scrollBy(0.75 * (fadeBg.isHorizontal ? scrollX : scrollY));
+    bgPane.scrollBy(0.25 * scrollX, 0.25 * scrollY);
+
+    var unscrolled = levelPane.scrollBy(scrollX, scrollY);
+    if (unscrolled.x !== 0) {
+        sprites[0].x += unscrolled.x;
+    }
+    if (unscrolled.y !== 0) {
+        sprites[0].y += unscrolled.y;
+    }
+
+    d('Sprite-Pos', sprites[0].x, ',', sprites[0].y);
+}
+
+onkeydown = function(e) {
+    keys[e.key] = e.key;
+};
+
+onkeyup = function(e) {
+    delete keys[e.key];
+};
