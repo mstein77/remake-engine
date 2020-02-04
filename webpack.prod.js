7976219bd746ad2460e23f040147c5bd9537bb14
Stein <marc.stein@m-c02pj18rg3qn.fritz.box>
2020-02-04 23:06:14 +0100

    Copy static files for build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,13 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new CopyPlugin([
+            {from: 'src/public/audio', to: 'audio'},
+            {from: 'src/public/css', to: 'css'},
+        ])
+    ]
 });
