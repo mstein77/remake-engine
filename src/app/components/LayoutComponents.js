f943eb11765b431751312b609d38b7ce4da1bf02
mstein77 <marc.stein@gfk.com>
2021-01-10 10:18:50 +0100

    Layout guide

---
+++ src/app/components/LayoutComponents.js
@@ -0,0 +1,40 @@
+import React, {Fragment, useState, useRef, useEffect, useContext, useMemo} from "react";
+import {d} from '../helper/helper';
+
+function Content() {
+    return (
+        <div></div>
+    )
+}
+
+function Stack() {
+    return (
+        <div></div>
+    )
+}
+
+function Grid() {
+    return (
+        <div></div>
+    )
+}
+
+function Overlays() {
+    return (
+        <div></div>
+    )
+}
+
+function Overlay() {
+    return (
+        <div></div>
+    )
+}
+
+export {
+    Content,
+    Stack,
+    Grid,
+    Overlays,
+    Overlay
+}
