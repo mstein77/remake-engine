74b85ac8fdad6e0b5fcc545182f3fcb2c00be5f5
Marc Stein <marc.stein@gfk.com>
2022-08-28 17:45:22 +0200

    New library build config

---
+++ webpack.build-engine.cjs
@@ -0,0 +1,102 @@
+const path = require('path');
+
+const distPath = path.resolve(__dirname, 'dist-engine');
+
+const glob = require("glob");
+const {DefinePlugin} = require("webpack");
+
+const devMode = process.env.NODE_ENV !== "production";
+
+const globPath = './src/engine/panes/*/config.js';
+const paneFiles = glob.sync(globPath);
+
+const config = env => {
+    return {
+        entry: {
+            engine: './src/engine/index.js'
+        },
+        output: {
+            path: distPath,
+            clean: true,
+            filename: '[name].js',  // Name of generated bundle after build
+            library: {
+                name: '2dfireengine',
+                type: 'umd',
+            }
+        },
+        optimization: {
+            // Instruct webpack not to obfuscate the resulting code
+            minimize: false,
+            splitChunks: {
+                chunks: 'all',
+                minSize: 0,
+                cacheGroups: {
+                    vendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name(module, chunks, cacheGroupKey) {
+                            const packageName = module.context.match(
+                                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+                            )[1];
+                            return `${cacheGroupKey}.${packageName.replace("@", "")}`;
+                        }
+                    },
+                    common: {
+                        minChunks: 2,
+                        priority: -10
+                    }
+                }
+            },
+            runtimeChunk: "single"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    use: 'babel-loader',
+                    exclude: [
+                        /2dfireengine\/node_modules/
+                    ]
+                },
+                {
+                    test: /\.css$/i,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader'
+                    ],
+                }
+            ]
+        },
+        plugins: [  // Array of plugins to apply to build chunk
+            /*
+            new DefinePlugin({
+                BASE_URL: JSON.stringify(process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:8080')
+            }),
+            new HtmlWebpackPlugin({
+                template: __dirname + "/src/public/index.html",
+                inject: 'body',
+                title: 'Production'
+            }),
+            new MiniCssExtractPlugin(),
+            // new BundleAnalyzerPlugin({}),
+            new StatoscopeWebpackPlugin({
+                open: true
+            }),
+
+             */
+        ],
+        resolve: {extensions: ['*', '.js', '.jsx']},
+        mode:
+//        'production'
+            'development'
+        ,
+        /*
+        devtool: false,
+        devServer: {  // configuration for webpack-dev-server
+            contentBase: distPath,  //source of static assets
+            port: 7700, // port to run dev-server
+        }
+         */
+    }
+};
+
+module.exports = config;
