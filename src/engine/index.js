280250e57110bc22b776e443cc75b9d45fe20a68
Marc Stein <marc.stein@gfk.com>
2022-09-03 11:49:54 +0200

    BUGFIX lazy lading pane editor

--- src/engine/index.js
+++ src/engine/index.js
@@ -3,4 +3,4 @@ import './core/core.css';
 export { Game, Screen } from "./core/classes.js";
 export { d } from "./helper/helper.js";
 export { BackgroundPane } from "./panes/BackgroundPane/pane.js";
-
+export { PatternPane } from "./panes/PatternPane/pane.js";
