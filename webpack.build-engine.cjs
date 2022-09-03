9c1d748bb031e6894f0f45a678ed199e28dc79c2
Marc Stein <marc.stein@gfk.com>
2022-09-03 17:22:22 +0200

    BUGFIX woff loader in wrong build

--- webpack.build-engine.cjs
+++ webpack.build-engine.cjs
@@ -79,6 +79,10 @@ const config = env => {
                 {
                     test: /\.(css)$/,
                     use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: 'asset/resource',
                 }
             ]
         },
--- webpack.build-game-dev.cjs
+++ webpack.build-game-dev.cjs
@@ -99,10 +99,6 @@ const buildConfig = env => {
                 {
                     test: /\.(css)$/,
                     use: ['style-loader', 'css-loader']
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                    type: 'asset/resource',
                 }
             ]
         },
