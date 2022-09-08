eacbbb26bf87c4c8450050560c6a641508b9fffe
Marc Stein <marc.stein@gfk.com>
2022-09-08 22:03:51 +0200

    BUGFIX no loader for fonts given in game build

--- webpack.build-game-dev.cjs
+++ webpack.build-game-dev.cjs
@@ -112,6 +112,10 @@ const buildConfig = env => {
                 {
                     test: /\.(css)$/,
                     use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: 'asset/resource',
                 }
             ]
         },
