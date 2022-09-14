9c55a42541a88dbaa9266d1da43a6afdae6eea69
Marc Stein <mstein77@gmx.de>
2022-09-14 21:56:11 +0200

    Add shared helper as cjs module

--- src/server/setupMiddlewares.cjs
+++ src/server/setupMiddlewares.cjs
@@ -2,7 +2,7 @@ const path = require("path");
 const cors = require('cors')
 const express = require('express');
 const fs = require('fs');
-const { isValidResourceId, getRelevantResources, ResourceDependencies } = require('../app/helper/helper.js');
+const { isValidResourceId, getRelevantResources, ResourceDependencies } = require('../engine/helper/shared.cjss');
 
 const DIST_DIR = path.join(__dirname, "dist");
 const STATIC_DIR = path.join(__dirname, "src/public");
