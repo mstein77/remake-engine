e627aa3a4cc9a8b3994012e22b13147e6d9dcd2c
Marc Stein <mstein77@gmx.de>
2022-09-18 09:47:07 +0200

    BUGFIX redeclaration of const

--- webpack.build-common.cjs
+++ webpack.build-common.cjs
@@ -6,7 +6,6 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const TerserPlugin = require("terser-webpack-plugin");
 const StatoscopeWebpackPlugin = require('@statoscope/webpack-plugin').default;
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
